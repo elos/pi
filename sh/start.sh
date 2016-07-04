7f66d9cd1a281c6a0a5d2f5f0f12637298412490
Nick Landolfi <nclandolfi@gmail.com>
2016-07-03 23:47:40 -0700

    add extra .

--- sh/start.sh
+++ sh/start.sh
@@ -5,6 +5,6 @@ set -e
 mkdir -p /tmp/grovepi
 
 ./stop.sh
-go run ./grovepi/cmd/grovepi/main.go -config /tmp/grovepi/config > /tmp/grovepi/stdout.txt 2> /tmp/grovepi/stderr.txt &
+go run ../grovepi/cmd/grovepi/main.go -config /tmp/grovepi/config > /tmp/grovepi/stdout.txt 2> /tmp/grovepi/stderr.txt &
 echo $! > /tmp/grovepi/pid
 echo "Started"
