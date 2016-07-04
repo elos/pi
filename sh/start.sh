73e433ef19e0d1453efbab23da3650c8575cdd5d
Nick Landolfi <nclandolfi@gmail.com>
2016-07-03 23:43:34 -0700

    change start.sh from sense.go to new infra

--- sh/start.sh
+++ sh/start.sh
@@ -3,6 +3,6 @@
 set -e
 
 ./stop.sh
-go run sense.go > /var/grovepi/stdout.txt 2> /var/grovepi/stderr.txt &
+go run ./grovepi/cmd/grovepi/main.go -config /tmp/grovepi.config > /var/grovepi/stdout.txt 2> /var/grovepi/stderr.txt &
 echo $! > /var/grovepi/pid
 echo "Started"
